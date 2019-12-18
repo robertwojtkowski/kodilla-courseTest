ebb92f9a15a727921e328ffe2886b5f565d4e01e
Michal <m.mrozek2@pwpw.pl>
2019-12-18 12:54:02 +0100

    zadanie 6.6

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,55 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+//import com.kodilla.stream.book.Book;
+//import com.kodilla.stream.book.BookDirectory;
+//import com.kodilla.stream.forumuser.Forum;
+//import com.kodilla.stream.forumuser.ForumUser;
+//import com.kodilla.stream.person.People;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.time.temporal.ChronoUnit.YEARS;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+
+
+
+
+        //7.1
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        String inputWord = "aBcDeFgHiJkLmNoPqRsTuWvXyZ";
+        System.out.print("converting \"" + inputWord + "\" to uppercase: ");
+        System.out.println(poemBeautifier.beautify(inputWord, String::toUpperCase));  //referencja do metody
+        System.out.print("converting \"" + inputWord + "\" to lowercase: ");
+        System.out.println(poemBeautifier.beautify(inputWord, str -> str.toLowerCase())); // wyrazenie lambda
+        System.out.print("first 5 letters of \"" + inputWord + "\": ");
+        System.out.println(poemBeautifier.beautify(inputWord, str -> str.substring(0,5)));
+        //System.out.println(poemBeautifier.beautify(inputWord, (str, f, s)-> str.substring(f,s)));
+        System.out.print("last 5 letters of \"" + inputWord + "\": ");
+        System.out.println(poemBeautifier.beautify(inputWord, str -> str.substring(str.length()-5)));
+        System.out.print("adding preffix and suffix to \"" + inputWord + "\": ");
+        System.out.println(poemBeautifier.beautify(inputWord, str -> "~~~" + str + "~~~"));
+
+
+        //7.3
+
+        Forum theUserList = new Forum();
+        Map<Integer, ForumUser> theResultMap = theUserList.getUserList().stream()
+                .filter(user -> user.getSex() == 'M')
+                .filter(user -> YEARS.between(user.getBirthDate(), LocalDate.now()) >= 20)
+                .filter(user -> user.getPostQuantity() > 0)
+                .collect(Collectors.toMap(ForumUser::getUserID, user -> user));
+        System.out.println("# elements: " + theResultMap.size());
+        theResultMap.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())git
+                .forEach(System.out::println);
+
+
+    }
+}
